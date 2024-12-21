921d499d55e3c9e1b0e2d6d83798056142d5dbc1
myovira <meetdurga123@gmail.com>
2024-12-21 13:40:09 +0530

    Todo-with redux logically completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import todo from "../src/assets/todo.png"
 import { useDispatch, useSelector } from 'react-redux'
-import { addTodo } from './features/Todo/todoSlice';
+import { addTodo, deleteTodo, doneTodo } from './features/Todo/todoSlice';
 import { nanoid } from '@reduxjs/toolkit';
 
 const App = () => {
@@ -9,127 +9,126 @@ const App = () => {
 
   const dispatch = useDispatch();
 
-  const todos = useSelector((state)=>state.todos.tasks);
-  
+  const todos = useSelector((state) => state.todos.tasks);
 
-function handleAddTodo() {
 
-  // console.log("Handling add function");
-  //{id: nanoid(), todo: "learn something new today", done:false}
-const todoObj = {
-  id: nanoid(), 
-  todo: task, 
-  done: false,
-  
-};
-  dispatch(addTodo(todoObj));
-  
-}
+
+  function handleAddTodo() {
+
+        const todoObj = {
+      id: nanoid(),
+      todo: task,
+      done: false,
+
+    };
+    dispatch(addTodo(todoObj));
+    settask("");
+  }
 
 
   return (
 
     <div className="bg-slate-600">
       <div className='flex justify-center items-center'>
-        <img src={todo} alt="Todo Image" className='w-40'/>
+        <img src={todo} alt="Todo Image" className='w-40' />
       </div>
       <h1 className="text-center text-3xl font-bold text-yellow-300">
-      The Best Todo Application
+        The Best Todo Application
       </h1>
-      
-    <div className="flex justify-center items-center py-10 gap-8">
-      <input
-        type="text"
-        placeholder="Enter Todo"
-        className="px-10 py-4 rounded-md hover:bg-blue-200"
-        onChange={(e)=> settask(e.target.value)}
-      />
-      <button
-        className="border border-purple-500 px-10 py-4 rounded-md font-bold text-purple-400
-      hover:bg-slate-500"
-      onClick={()=> handleAddTodo()}
-      > 
-        Add To do
-      </button>
-    </div>
-    <div className="flex justify-between px-40 min-h-[70vh]">
-      <div className="flex flex-col gap-3 w-1/2">
 
-        {todos.map((todo) => {
+      <div className="flex justify-center items-center py-10 gap-8">
+        <input
+          type="text"
+          placeholder="Enter Todo"
+          className="px-10 py-4 rounded-md hover:bg-blue-200"
+          onChange={(e) => settask(e.target.value)}
+          value={task}
+        />
+        <button
+          className="border border-purple-500 px-10 py-4 rounded-md font-bold text-purple-400
+      hover:bg-slate-500"
+          onClick={() => handleAddTodo()}
+        >
+          Add To do
+        </button>
+      </div>
+      <div className="flex justify-between px-32 w-full min-h-[70vh]">
+        <div className="flex flex-col gap-4 w-1/2">
 
-          return (
-          <TodoComponent
-          key= {todo.id}
-          title= {todo.todo}
-          id= {todo.id}
-          done= {todo.done}
-           />
-          );
-      })}
-</div> 
-{/* 
-         <div className="flex gap-8 text-slate-50 items-center">
-          <p>Learn React js and redux </p>
-          <button
-            className="border border-blue-500 px-4 py-2 rounded-md font-bold text-blue-500
-      hover:bg-slate-500"
-          >
-            Done
-          </button>
-          <button
-            className="border border-pink-400 px-4 py-2 rounded-md font-bold text-pink-400
-      hover:bg-slate-500"
-          >
-            Delete
-          </button>
-          </div>
-   */}
+          {todos.map((todo) => {
+
+            if (!todo.done) {
+              return (
+                <TodoComponent
+                  key={todo.id}
+                  title={todo.todo}
+                  id={todo.id}
+                  done={todo.done}
+                />
+              );
+             }
+          })}
+        </div>
+       
+        <div className="w-[2px] bg-slate-400 mx-5"></div>
+        <div className="flex flex-col gap-4 w-1/2">
+          {todos.map((todo) => {
+
+            if (todo.done) {
+              return (
+                <TodoComponent
+                  key={todo.id}
+                  title={todo.todo}
+                  id={todo.id}
+                  done={todo.done}
+                />
+              );
+
+            }
 
 
-        
-      <div className="w-[2px] border-yellow-100 border-dotted bg-slate-400"></div>
-      <div className="flex flex-col">
-        <div className="flex gap-8 text-slate-50 items-center">
-          <p>Learn HTML, CSS and Js</p>
-          <button
-            className="border border-blue-500 px-4 py-2 rounded-md font-bold text-blue-500
-      hover:bg-slate-500"
-          >
-            Incomplete
-          </button>
-          <button
-            className="border border-pink-400 px-4 py-2 rounded-md font-bold text-pink-400
-      hover:bg-slate-500"
-          >
-            Delete
-          </button>
+          })}
         </div>
       </div>
     </div>
-  </div>
+ 
     
   )
 
-  function TodoComponent ({ title, id, done}) {
+function TodoComponent({ title, id, done }) {
 
-    return (
-      <div className="flex gap-8 text-slate-50 items-center">
+  const dispatch = useDispatch();
+
+  function handleDeleteTodo(id) {
+
+    dispatch(deleteTodo(id));
+
+  }
+
+  function handledoneTodo(id){
+    dispatch(doneTodo(id));
+  }
+
+  return (
+    <div className="flex gap-8 text-slate-50 items-center">
       <p>{title}</p>
       <button
         className="border border-blue-500 px-4 py-2 rounded-md font-bold text-blue-500
   hover:bg-slate-500"
+   onClick={() => handledoneTodo(id)}
       >
         Done
       </button>
       <button
         className="border border-pink-400 px-4 py-2 rounded-md font-bold text-pink-400
   hover:bg-slate-500"
+        onClick={() => handleDeleteTodo(id)}
       >
         Delete
       </button>
-      </div>
-    )
-  }
+    </div>
+  )
+}
     
 }
 
--- src/features/Todo/todoSlice.js
+++ src/features/Todo/todoSlice.js
@@ -9,13 +9,29 @@ const todoSlice=createSlice({
 
         addTodo: (state, action)=>{
 
-            console.log("action looks like",action);
+            //console.log("action looks like",action);
             state.tasks.push(action.payload);//{id: nanoid(), todo: "learn something new today", done:false}
         },
 
+        deleteTodo : (state, action)=>{
+           const id =action.payload;
 
+           state.tasks=state.tasks.filter(task =>task.id !== id);
+
+        },
+
+        doneTodo : (state, action) =>{
+            const id =action.payload;
+
+            state.tasks = state.tasks.map(task =>{
+                 if (task.id ===id){
+                    task.done =!task.done;
+                 }
+                 return task;
+            });
+        },
     },
 });
 
-export const{addTodo}=todoSlice.actions;
+export const{addTodo, deleteTodo, doneTodo}=todoSlice.actions;
 export const todoReducer=todoSlice.reducer;
